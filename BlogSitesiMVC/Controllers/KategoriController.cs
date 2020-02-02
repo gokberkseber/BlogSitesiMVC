6d3aec28faf0b0dec431df2d731ccabe17e0e3f9
Gokberk Seber <gokberkseber@hotmail.com>
2020-02-02 16:36:34 +0300

    en son kategori kontroller eklendi

---
+++ BlogSitesiMVC/Controllers/KategoriController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSitesiMVC.Controllers
+{
+    using Models;
+    public class KategoriController : Controller
+    {
+        BlogDBContext context = new BlogDBContext();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult KategoriWidget()
+        {
+            return PartialView(context.Kategori.ToList());
+        }
+    }
+}
